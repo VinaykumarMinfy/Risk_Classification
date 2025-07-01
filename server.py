b7193c09711959144cb7e3e7b87e0fbf6ea7fc4b
VinaykumarMinfy <vinaykumar.mattapally@minfytech.com>
2025-07-02 01:01:07 +0530

    final

---
+++ server.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def serve_model(model_uri="models:/BankLoanBestModel/Production", host="127.0.0.1", port=5001):
+    try:
+        subprocess.run([
+            "mlflow", "models", "serve",
+            "-m", model_uri,
+            "--host", host,
+            "--port", str(port),
+            "--env-manager=local"
+        ], check=True)
+    except subprocess.CalledProcessError as e:
+        print("❌ Failed to start MLflow server:", e)
+
+serve_model()
